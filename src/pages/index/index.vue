72ada7fc05ef17013817fc898472533426ec3f85
yin_guohan <yin_guohan@nhn-st.com>
2026-01-21 08:40:00 +0800

    生成二维码按钮

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,6 +2,9 @@
   <view class="up-vid">
     <UploadVideo />
   </view>
+  <view class="btn-submit">
+    <button class="btn-generate-qrCode">生成二维码</button>
+  </view>
 </template>
 
 <script setup>
@@ -9,10 +12,23 @@ import UploadVideo from "../../components/uploadVideo.vue";
 </script>
 
 <style scoped>
-  .up-vid{
-    display: flex;
-    justify-content: center;
-    /* 核心：向下平移屏幕高度的20% */
+
+  /* 上传视频+号按钮 */
+.up-vid {
+  display: flex;
+  justify-content: center;
+  /* 核心：向下平移屏幕高度的20% */
   transform: translateY(15vh);
-  }
+}
+/* 生成二维码按钮 */
+.btn-generate-qrCode {
+  width: 70%;
+  background-color: #007aff;
+  color: #ffffff;
+}
+/* 控制按钮位置 */
+.btn-submit{
+  display: flex;
+  margin-bottom: 20%
+}
 </style>
